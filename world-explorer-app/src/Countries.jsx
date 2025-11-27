084b22a160bb3b577fc31ab09df9cf63f2a38298
yaredgebre <yaredgebre120@gmail.com>
2025-11-27 08:32:38 -0300

    setting the countires page

---
+++ world-explorer-app/src/Countries.jsx
@@ -0,0 +1,160 @@
+import React, { useEffect, useState } from "react";
+import Countrydata from "./CustomHook";
+import "./Home.css";
+import HomePage from "./Home";
+
+function CountriesPage() {
+  const [urladress, seturladress] = useState(
+    "https://restcountries.com/v3.1/all?fields=name,flags,languages"
+  );
+
+  const { data, is_error, Loading } = Countrydata(urladress);
+    console.log(data)
+  const [continentName1, setcontinentName] = useState("");
+  const [searchName, setSearchName] = useState("");
+  const [homepage, sethomepage] = useState(false);
+
+  useEffect(() => {
+    if (is_error) alert(`There is an error: ${is_error}`);
+  }, [is_error]);
+   if(homepage){
+    return <HomePage/>
+   }
+  function filterByContinent() {
+    seturladress(
+      `https://restcountries.com/v3.1/lang/${continentName1}?fields=name,flags,languages`
+
+    );
+  }
+
+  function inputContinent(e) {
+    setcontinentName(e.target.value);
+  }
+
+
+  function Searchbyname(e) {
+    setSearchName(e.target.value);
+  }
+
+  function Searchfunction(e) {
+    e.preventDefault();
+    seturladress(
+      `https://restcountries.com/v3.1/name/${searchName}?fields=name,flags,languages`
+    );
+  }
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            World-Explorer-App
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <button className="nav-link active" aria-current="page" onClick={()=>{
+                  sethomepage(true);
+                }}>
+                  Home
+                </button>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Quiz
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Settings
+                </a>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle active"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="true"
+                >
+                  Filter
+                </a>
+
+                <ul className="dropdown-menu">
+                  <input
+                    placeholder="Enter Continent"
+                    name="continent"
+                    onChange={inputContinent}
+                  />
+                  <li>
+                    <button
+                      className="dropdown-item"
+                      onClick={filterByContinent}
+                    >
+                      Check language
+                    </button>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search by name"
+                onChange={Searchbyname}
+              />
+              <button
+                className="btn btn-outline-success"
+                type="button"
+                onClick={Searchfunction}
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      <div className="grid-content">
+        {data.slice(0, 100).map((country, index) => (
+          <div className="grid-elements" key={index}>
+            <ul style={{ listStyle: "none" }}>
+              <li>Name: {country.name.official}</li>
+              <li style={{backgroundImage: `url(${country.flags.png})`,
+                          width: '30px',
+                          height: '20px',
+                          objectFit: "cover"
+                                }}></li>
+              <li>Language: {Object.values(country.languages)[0]}</li>
+              
+            </ul>
+
+            <button className="grid-button">view detail</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default CountriesPage;
+
+
--- world-explorer-app/src/Home.jsx
+++ world-explorer-app/src/Home.jsx
@@ -1,102 +1,179 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import Countrydata from "./CustomHook";
 import "./Home.css";
-function HomePage(){
-  const[urladress, seturladress] = useState("https://restcountries.com/v3.1/all?fields=name,region,population")
- const {data, is_error,Loading} = Countrydata(urladress);
- const[subRegionData, setBySubRegion] = useState();
- const[continentName1, setcontinentName] = useState();
- const[searchName, setSearchName] = useState()
- 
- function filterByContinent(){
-     seturladress(`https://restcountries.com/v3.1/region/${continentName1}?fields=name,region,population`);
- }
- function inputContinent(e){
-   const continentName = e.target.value;
-   setcontinentName(continentName);
- }
-function filterBySubregion(){
-     seturladress(`https://restcountries.com/v3.1/subregion/${subRegionData}?fields=name,region,population`);
- }
- function inputSubregion(e){
-   const subRegion = e.target.value;
-   setBySubRegion(subRegion);
- }
- function filterBySubregion(){
-     seturladress(`https://restcountries.com/v3.1/subregion/${subRegionData}?fields=name,region,population`);
- }
-  function Searchbyname (e){
-   const countryName = e.target.value;
-  setSearchName(countryName);
- }
- function Searchfunction(){
-  seturladress(`https://restcountries.com/v3.1/name/${searchName}?fields=name,region,population`)
- }
-  return <div><div>
-  <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">World-Explorer-App</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Countries</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" href="#">Quiz</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Setting</a>
-        </li>
-        <li className="nav-item dropdown">
-  <a 
-    className="nav-link dropdown-toggle active" 
-    href="#" 
-    role="button" 
-    data-bs-toggle="dropdown" 
-    aria-expanded="true"
-  >
-    Filter
-  </a>
+import CountriesPage from "./Countries";
 
-  <ul className="dropdown-menu">
-       <input placeholder="please enter the name" name="continent" onChange={inputContinent}/>
-    <li><button className="dropdown-item" href="#" onClick={filterByContinent}> Check Continent</button>
- 
-    </li>
-     <input placeholder="please enter the number" name="population" onChange={inputSubregion}/>
-    <li><button className="dropdown-item" href="#" onClick={filterBySubregion}>Check Sub-Region </button></li>
-     
-  </ul>
-</li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" onChange={Searchbyname} aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit" onClick={Searchfunction}>Search</button>
-      </form>
+function HomePage() {
+  const [urladress, seturladress] = useState(
+    "https://restcountries.com/v3.1/all?fields=name,region,population"
+  );
+
+
+  const { data, is_error, Loading } = Countrydata(urladress);
+   
+  const [subRegionData, setBySubRegion] = useState("");
+  const [continentName1, setcontinentName] = useState("");
+  const [searchName, setSearchName] = useState("");
+  const [countirespage, setcountriespage] = useState(false);
+
+  useEffect(() => {
+    if (is_error) alert(`There is an error: ${is_error}`);
+  }, [is_error]);
+   if(countirespage){
+    return <CountriesPage/>
+   }
+  function filterByContinent() {
+    seturladress(
+      `https://restcountries.com/v3.1/region/${continentName1}?fields=name,region,population`
+    );
+  }
+
+  function inputContinent(e) {
+    setcontinentName(e.target.value);
+  }
+
+  function inputSubregion(e) {
+    setBySubRegion(e.target.value);
+  }
+
+  function filterBySubregion() {
+    seturladress(
+      `https://restcountries.com/v3.1/subregion/${subRegionData}?fields=name,region,population`
+    );
+  }
+
+  function Searchbyname(e) {
+    setSearchName(e.target.value);
+  }
+
+  function Searchfunction(e) {
+    e.preventDefault();
+    seturladress(
+      `https://restcountries.com/v3.1/name/${searchName}?fields=name,region,population`
+    );
+  }
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            World-Explorer-App
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <button className="nav-link active" aria-current="page" onClick={()=>{
+                  setcountriespage(true);
+                }}>
+                  Countries
+                </button>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Quiz
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Settings
+                </a>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle active"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="true"
+                >
+                  Filter
+                </a>
+
+                <ul className="dropdown-menu">
+                  <input
+                    placeholder="Enter Continent"
+                    name="continent"
+                    onChange={inputContinent}
+                  />
+                  <li>
+                    <button
+                      className="dropdown-item"
+                      onClick={filterByContinent}
+                    >
+                      Check Continent
+                    </button>
+                  </li>
+
+                  <input
+                    placeholder="Enter Subregion"
+                    name="subregion"
+                    onChange={inputSubregion}
+                  />
+                  <li>
+                    <button
+                      className="dropdown-item"
+                      onClick={filterBySubregion}
+                    >
+                      Check Subregion
+                    </button>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search by name"
+                onChange={Searchbyname}
+              />
+              <button
+                className="btn btn-outline-success"
+                type="button"
+                onClick={Searchfunction}
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      <div className="grid-content">
+        {data.slice(0, 100).map((country, index) => (
+          <div className="grid-elements" key={index}>
+            <ul style={{ listStyle: "none" }}>
+            <li>Name: {country.name.official}</li>
+              <li>Region: {country.region}</li>
+              <li>Population: {country.population}</li>
+            </ul>
+
+            <button className="grid-button">view detail</button>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-</nav>
-</div>
-
-<div className="grid-content">
-{data.slice(0, 100).map((country,index) => (
-  <div className="grid-elements" key={index}>
-  <ul style={{listStyle:'none'}}>
-    <li>Name: {country.name.official}</li>
-    <li>Continent: {country.region}</li>
-    <li>Population: {country.population}</li>
-  </ul>
-  <button className="grid-button"> view detail</button>
-  </div>
-))}
-
-</div>
-
-
-</div>
+  );
 }
+
 export default HomePage;
+
+
