e1de5be27b995f54ddf4c771249232f1f94ff7c4
yaredgebre <yaredgebre120@gmail.com>
2025-11-28 06:45:33 -0300

    prevent defaulting the toggle function

--- world-explorer-app/src/TeamContext.jsx
+++ world-explorer-app/src/TeamContext.jsx
@@ -1,12 +1,20 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState("light"); 
 
-  function toggleTheme() {
-    setTheme(prev => prev === "light" ? "dark" : "light");
+
+  function toggleTheme(e) {
+   e.preventDefault(); 
+    setTheme(prev =>
+      {
+        const newtheme = prev === "light" ? "dark" : "light";
+        return newtheme
+      
+      });
+
   }
 
   return (
