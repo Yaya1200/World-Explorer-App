7abd30d21817b582d2d01c507aaf4f6a59b6e6a2
yaredgebre <yaredgebre120@gmail.com>
2026-02-14 04:07:53 -0300

    adding routers to app

--- world-explorer-app/src/App.jsx
+++ world-explorer-app/src/App.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import './App.css';
 import HomePage from './Home';
 import { ThemeProvider } from './TeamContext';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CountriesPage from './Countries';
+import QuizPage from './Quiz';
 
 
 function App() {
   return (
+    <BrowserRouter>
      <ThemeProvider>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/countries" element={<CountriesPage />}/>
+        <Route path="/quiz" element={<QuizPage />}/>
+      </Routes>
+      
       </ThemeProvider>
+      </BrowserRouter>
   
    
   );
